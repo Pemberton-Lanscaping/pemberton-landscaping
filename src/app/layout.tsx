b5e5fc9798c44ad8c9ccba7f49f8e75b8b9269ad
Dragon Prevost <dragon@dkp.io>
2024-08-08 18:08:41 -0700

    Added title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pemberton Landscaping",
-  description: "Providing landscaping construction, installations, and maintenance in the sea to sky corridor.",
+  description: "Landscaping construction, installations, and maintenance in the sea to sky corridor.",
 };
 
 export default function RootLayout({
@@ -16,6 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Pemberton Landscaping</title>
+        <meta name="description" content="Landscaping construction, installations, and maintenance in the sea to sky corridor."/>
+        <meta property="og:title" content="Pemberton Landscaping" />
+        <meta property="og:description" content="Landscaping construction, installations, and maintenance in the sea to sky corridor."/>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
